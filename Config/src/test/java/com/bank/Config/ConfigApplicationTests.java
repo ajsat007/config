ebc8f233b784d20293e3bc0efa20c496268c1d6e
ajsat007 <87180291+ajsat007@users.noreply.github.com>
2023-03-23 19:18:24 +0530

    Add files via upload

---
+++ Config/src/test/java/com/bank/Config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.Config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ConfigApplication.class)
+class ConfigApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
